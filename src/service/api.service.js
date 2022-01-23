600d0de289ead6d8776e3b23872895ff8b597a76
Vuradisurafu <ruozversa@yandex.ru>
2022-01-23 15:04:15 +0300

    Show list posts

--- src/service/api.service.js
+++ src/service/api.service.js
@@ -9,13 +9,28 @@ class ApiService {
                 method: 'post',
                 body: JSON.stringify(post)
             })
-           const response = await fetch(request) 
-           return await response.json()
+           return useRequest(request);
         } catch (error) {
             console.error(error);
         }
        
     }
+
+    async fetchPosts() {
+        try {
+            const request = new Request(`${this.url}/posts.json`, {
+                method: 'get'
+            })
+            return useRequest(request)
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+async function useRequest(request) {
+    const response = await fetch(request);
+    return await response.json();
 }
 
 export const apiService = new ApiService('https://mini-blog-js-4887c-default-rtdb.firebaseio.com'); 
